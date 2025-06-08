75f130b1ec56d78d22e86c7efb9038272013430e
daria <kapoor.darya@gmail.com>
2025-06-09 00:27:27 +0300

    add lesson03

---
+++ src/by/it/group410972/kapur/lesson03/Lesson03Test.java
@@ -0,0 +1,46 @@
+package by.it.group410972.kapur.lesson03;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.InputStream;
+import org.junit.Test;
+
+public class Lesson03Test {
+    /*
+    для прохождения тестов создайте JUnit-конфигурацию на свой пакет:
+    Поля:
+    Name:               Test kapur (тут ваша фамилия)
+    Test kind:          All in package
+    Package:            by.it.kapur (тут ваша фамилия)
+    Search for test:    In whole project
+    */
+
+
+    @Test
+    public void checkA() throws Exception {
+        InputStream inputStream = A_Huffman.class.getResourceAsStream("dataA.txt");
+        A_Huffman instance = new A_Huffman();
+        String result = instance.encode(inputStream);
+        boolean ok = result.equals("01001100100111");
+        assertTrue("A failed", ok);
+    }
+
+    @Test
+    public void checkB() throws Exception {
+        InputStream inputStream = B_Huffman.class.getResourceAsStream("dataB.txt");
+        B_Huffman instance = new B_Huffman();
+        String result = instance.decode(inputStream);
+        boolean ok = result.equals("abacabad");
+        assertTrue("B failed", ok);
+    }
+
+    @Test
+    public void checkC() throws Exception {
+        InputStream inputStream = C_HeapMax.class.getResourceAsStream("dataC.txt");
+        C_HeapMax instance = new C_HeapMax();
+        Long res = instance.findMaxValue(inputStream);
+        boolean ok = (res == 500);
+        assertTrue("C failed", ok);
+    }
+
+}
